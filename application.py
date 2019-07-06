deb3c3d3e88457db7a52cc4276e26c84a0aaf88b
Michael-Choi <michael.choi@queensu.ca>
2019-07-05 20:47:31 -0400

    Setting up login form and database table creation

--- application.py
+++ application.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+#engine used to manage connections to the database user environment variable
+engine=create_engine(os.getenv("DATABASE_URL"))
+
+#create scoped session so individuals operate on separate sessions
+db=scoped_session(sessionmaker(bind=engine))
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html",text="testing abcd")
+    return render_template("login.html")
 
-@app.route("/<string:name>")
-def hello(name):
-    return f"Hello, {name}!"
+@app.route("/login", methods=["POST"])
+def login():
+    username=request.form.get("username")
+    password=request.form.get("password")
+
+    #pseudocode check if username/password is in database if successful send them to success page
+    #if username/password isn't in database redirect them to login.html and tell them to try again
+
+    return render_template("success.html", username=username, password=password)
